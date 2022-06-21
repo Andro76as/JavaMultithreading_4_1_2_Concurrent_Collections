9892d052f8a523326a1dea30de559991da50cfa5
Andro76as <andro76as@gmail.com>
2022-06-21 20:29:53 +0300

    Commit1

--- src/main/java/ru/netology/CallCenter.java
+++ src/main/java/ru/netology/CallCenter.java
@@ -1,2 +1,40 @@
-package ru.netology;public class CallCenter {
+package ru.netology;
+
+import java.util.Queue;
+import java.util.concurrent.ArrayBlockingQueue;
+
+public class CallCenter {
+
+    private static final int calls = 60;
+    private static final Queue<String> phoneCalls = new ArrayBlockingQueue<>(calls);
+
+    public void call() {
+        try {
+            for (int i = 0; i < calls; i++) {
+                phoneCalls.offer("Звонок №" + i);
+                System.out.println("Поступил звонок №" + i);
+                Thread.sleep(100);
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void takeTheCall() {
+        while (!phoneCalls.isEmpty()) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            if (phoneCalls.peek() != null) {
+                System.out.println("Оператор " + Thread.currentThread().getName() + " ответил на звонок " + phoneCalls.poll());
+            } else {
+                Thread.currentThread().interrupt();
+                System.out.println("Оператор закончил работу, звонки кончились");
+            }
+
+        }
+    }
+
 }
--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -1,5 +1,18 @@
-package ru.netology;public class Main {
+package ru.netology;
+
+public class Main {
+    public static final int specialist = 4;
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        CallCenter center = new CallCenter();
+//        for (int i = 1; i <= calls; i++) {
+//            new Thread(null, center::call, "№ " + i).start();
+//        }
+        new Thread(null, center::call, "Поток АТС").start();
+
+        for (int i = 1; i <= specialist; i++) {
+            new Thread(null, center::takeTheCall, "Оператор №" + i).start();
+        }
+
     }
 }
